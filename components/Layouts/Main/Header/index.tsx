59a94ebbc4945b02e4b03ab86bf68b91e6fcbf68
Sarah Abed Elrazeq <sarah.abed.elrazeq@gmail.com>
2023-01-14 20:20:26 +0300

    layout

--- components/Layouts/Main/Header/index.tsx
+++ components/Layouts/Main/Header/index.tsx
@@ -1,5 +1,41 @@
 import React from "react";
+import Link from "next/link";
+import {
+  Breadcrumbs,
+  Link as LinkComponent,
+  Typography,
+  Box,
+} from "@mui/material";
+import { NavigateNext } from "@mui/icons-material";
+interface Props {
+  title?: string;
+  links: Array<{ link: string; label: string }>;
+}
 
-export default function Header() {
-  return <div>index</div>;
+export default function Header({ links, title }: Props) {
+  return (
+    <Box
+      py={1}
+      sx={{ borderBottom: (theme) => `1px solid ${theme.palette.grey[400]}` }}
+      component="header"
+    >
+      <Breadcrumbs
+        separator={<NavigateNext fontSize="small" />}
+        aria-label="breadcrumb"
+      >
+        {links.map(({ link, label }, index) => (
+          <LinkComponent
+            component={Link}
+            underline="hover"
+            color="inherit"
+            href={link}
+            key={index}
+          >
+            {label}
+          </LinkComponent>
+        ))}
+        <Typography color="text.primary">{title}</Typography>
+      </Breadcrumbs>
+    </Box>
+  );
 }
