17ad92b10e75d8939a523a4e88bc0f70040df40d
Sarah Abed Elrazeq <sarah.abed.elrazeq@gmail.com>
2023-01-14 20:20:40 +0300

    DataTable

---
+++ components/DataTable/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "logId", headerName: "Log ID", width: 200 },
+  { field: "applicationType", headerName: "Application Type", width: 200 },
+  { field: "applicationId", headerName: "Application ID", width: 200 },
+  { field: "actionType", headerName: "Action", width: 200 },
+  {
+    field: "actionDetails",
+    headerName: "Action Details",
+    width: 200,
+  },
+  { field: "creationTimestamp", headerName: "Date: Time", width: 200 },
+];
+
+export default function DataTable({ rows }: { rows?: Array<object> }) {
+  return (
+    <div style={{ height: 631, width: "100%" }}>
+      <DataGrid
+        rows={rows || []}
+        columns={columns}
+        pageSize={10}
+        disableColumnMenu={true}
+        getRowId={(item) => item.logId}
+      />
+    </div>
+  );
+}
